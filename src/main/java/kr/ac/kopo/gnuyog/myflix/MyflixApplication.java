42b0c35d21d0e95bb3755242e9e9dd1da30e16d8
goldstargreat <goldstargreat@gmail.com>
2026-06-15 08:50:38 +0900

    Initial commit

---
+++ src/main/java/kr/ac/kopo/gnuyog/myflix/MyflixApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.gnuyog.myflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyflixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/gnuyog/myflix/MyflixApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.gnuyog.myflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
