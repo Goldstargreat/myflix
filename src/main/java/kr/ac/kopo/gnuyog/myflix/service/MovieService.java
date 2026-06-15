e899f1d09f08200ee6aeb9ff98e107901d4a91f9
goldstargreat <goldstargreat@gmail.com>
2026-06-15 12:39:51 +0900

    MovieService.java 정의

--- src/main/java/kr/ac/kopo/gnuyog/myflix/service/MovieService.java
+++ src/main/java/kr/ac/kopo/gnuyog/myflix/service/MovieService.java
@@ -8,18 +8,19 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+// 영화 관련 로직 처리 담당 클래스
 @Service
 public class MovieService
 {
-    @Autowired
+    @Autowired // Spring이 자동으로 MovieRepository 객체를 넣어줌
     private MovieRepository movieRepository;
-
+    // DB 작업을 여기서 직접 하지 않고 Repository에 위임
     public List<Movie> getAllMovies()
-    {
-        return movieRepository.findAll();
+    {  // 모든 영화 목록 가져오는 메서드
+        return movieRepository.findAll(); // DB 또는 저장소에서 전체 데이터 조회
     }
     public Optional<Movie> getMovieById(Long id)
-    {
+    {  // 영화 ID로 조회
         return movieRepository.findById(id);
     }
     public List<Movie> getMoviesByGenre(String genre)
@@ -27,12 +28,14 @@ public class MovieService
         return movieRepository.findByGenre(genre);
     }
     public Movie saveMovie(Movie movie)
-    {
+    {   // 새 영화 저장 또는 수정
         return movieRepository.save(movie);
-    }
+    }   // DB에 저장 요청
     public void deleteMovie(Long id)
-    {
+    {   // 영화 삭제 기능
         movieRepository.deleteById(id);
-    }
+    }  // ID 기준으로 삭제
 }
+
+// MovieRepository를 감싸서 컨트롤러에 서비스 형태로 제공하는 중간 계층
 
