02aacace20e7c0acad33e6311584ca61dcaf41c8
goldstargreat <goldstargreat@gmail.com>
2026-06-15 10:30:45 +0900

    MovieRepository.java 정의

--- src/main/java/kr/ac/kopo/gnuyog/myflix/repository/MovieRepository.java
+++ src/main/java/kr/ac/kopo/gnuyog/myflix/repository/MovieRepository.java
@@ -1,4 +1,15 @@
 package kr.ac.kopo.gnuyog.myflix.repository;
 
-public class MovieRepository {
+import kr.ac.kopo.gnuyog.myflix.model.Movie;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MovieRepository
+{
+    List<Movie> findAll();
+    Optional<Movie> findById(Long id);
+    List<Movie> findByGenre(String genre);
+    Movie save(Movie movie);
+    void deleteById(Long id);
 }
