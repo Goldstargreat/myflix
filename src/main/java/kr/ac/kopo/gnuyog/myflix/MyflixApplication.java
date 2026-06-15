bcf89a6bace91813f77e13431248ef8f2d7e6127
goldstargreat <goldstargreat@gmail.com>
2026-06-15 10:00:12 +0900

    MyflixApplication.java 애플리케이션 진입점 추가

--- src/main/java/kr/ac/kopo/gnuyog/myflix/MyflixApplication.java
+++ src/main/java/kr/ac/kopo/gnuyog/myflix/MyflixApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyflixApplication {
-
-    public static void main(String[] args) {
+public class MyflixApplication
+{
+    public static void main(String[] args)
+    {
         SpringApplication.run(MyflixApplication.class, args);
     }
-
 }
