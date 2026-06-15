92bc226fbc427d8f6efe38c6fb0626f2fba03870
goldstargreat <goldstargreat@gmail.com>
2026-06-15 10:54:16 +0900

    UserRepository.java 정의

--- src/main/java/kr/ac/kopo/gnuyog/myflix/repository/UserRepository.java
+++ src/main/java/kr/ac/kopo/gnuyog/myflix/repository/UserRepository.java
@@ -1,6 +1,12 @@
 package kr.ac.kopo.gnuyog.myflix.repository;
 
-public class UserRepository
-{
+import kr.ac.kopo.gnuyog.myflix.model.User;
 
+import java.util.Optional;
+
+public interface UserRepository
+{
+    Optional<User> findByUsername(String username);
+    User save(User user);
+    boolean existsByUsername(String username);
 }
