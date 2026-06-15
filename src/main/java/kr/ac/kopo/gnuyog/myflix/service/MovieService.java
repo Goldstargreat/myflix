649ca53d9219c4343d35fe5918655ad51abbd952
goldstargreat <goldstargreat@gmail.com>
2026-06-15 12:28:45 +0900

    MovieService.java 정의

--- src/main/java/kr/ac/kopo/gnuyog/myflix/service/MovieService.java
+++ src/main/java/kr/ac/kopo/gnuyog/myflix/service/MovieService.java
@@ -1,7 +1,38 @@
 package kr.ac.kopo.gnuyog.myflix.service;
 
+import kr.ac.kopo.gnuyog.myflix.model.Movie;
+import kr.ac.kopo.gnuyog.myflix.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MovieService
 {
+    @Autowired
+    private MovieRepository movieRepository;
 
+    public List<Movie> getAllMovies()
+    {
+        return movieRepository.findAll();
+    }
+    public Optional<Movie> getMovieById(Long id)
+    {
+        return movieRepository.findById(id);
+    }
+    public List<Movie> getMoviesByGenre(String genre)
+    {
+        return movieRepository.findByGenre(genre);
+    }
+    public Movie saveMovie(Movie movie)
+    {
+        return movieRepository.save(movie);
+    }
+    public void deleteMovie(Long id)
+    {
+        movieRepository.deleteById(id);
+    }
 }
 
