4cef13ddde42cfecdf70a1cfb834927027087bc0
goldstargreat <goldstargreat@gmail.com>
2026-06-15 12:11:58 +0900

    UserRepository.java 정의

--- src/main/java/kr/ac/kopo/gnuyog/myflix/repository/UserRepository.java
+++ src/main/java/kr/ac/kopo/gnuyog/myflix/repository/UserRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 
 public interface UserRepository
 {
+    // UserRepository 만드려면 아래와 같은 기능들을 반드시 구현하라
     Optional<User> findByUsername(String username);
-    User save(User user);
-    boolean existsByUsername(String username);
+    // username으로 사용자 찾기 있으면 User 반환, 없으면 비어있는 Optional 반환
+    User save(User user); // 유저 정보 저장
+    boolean existsByUsername(String username); // 아이디 존재 여부 확인
 }
